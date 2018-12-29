892b427a2805caa1e4918cd203b382d8ce0dea76
mnindrazaka <mnindrazaka@gmail.com>
2018-12-29 11:40:18 +0700

    add semantic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import React, { Component } from "react"
-import "./App.css"
+import { Button } from "semantic-ui-react"
 
 class App extends Component {
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React, React is fun
-          </a>
-        </header>
-      </div>
-    )
+    return <Button primary>Coba Klik</Button>
   }
 }
 
