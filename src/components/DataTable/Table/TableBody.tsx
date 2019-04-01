8755735c7c380671b29d8c78e377a23837035de1
mnindrazaka <mnindrazaka@gmail.com>
2019-04-01 11:38:04 +0700

    set cursor pointer on table row

--- src/components/DataTable/Table/TableBody.tsx
+++ src/components/DataTable/Table/TableBody.tsx
@@ -31,7 +31,10 @@ export default class TableBody extends Component<IProps> {
     return this.props.paginatedData.map((rowData, index) => (
       <Table.Row
         key={index}
-        onClick={() => this.props.onRowClick(JSON.parse(JSON.stringify(rowData)))}
+        onClick={() =>
+          this.props.onRowClick(JSON.parse(JSON.stringify(rowData)))
+        }
+        style={{ cursor: "pointer" }}
       >
         <Table.Cell>{startingNumber++}</Table.Cell>
         {this.renderCell(rowData)}
